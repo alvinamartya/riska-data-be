ccdadad1d409f073fb2edaa27813662b983fb8e0
Muhammad Taufiq hardiyanto Umar <taufiqophie@gmail.com>
2019-11-03 20:14:39 +0700

    add column district_id to users table

---
+++ database/migrations/2019_11_03_201127_add_district_id_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDistrictIdToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('district_id', 7)->index();
+            $table->foreign('district_id')->references('id')->on('district')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_district_id_foreign');
+            $table->dropColumn('district_id');
+        });
+    }
+}
