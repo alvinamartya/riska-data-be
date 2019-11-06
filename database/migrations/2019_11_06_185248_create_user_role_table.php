a725a153e5c10cc5277490052af095685eff7651
Fariz Hermawan <fariz.hermawan@traveloka.com>
2019-11-06 19:13:19 +0700

    Create Role and User Role table migration

---
+++ database/migrations/2019_11_06_185248_create_user_role_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRoleTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('user_role', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->unsignedSmallInteger('role_id');
+      $table->unsignedBigInteger('user_id');
+      $table->boolean("is_active");
+      $table->dateTime("expired_at")->nullable();
+
+      $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+      $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('user_role');
+  }
+}
