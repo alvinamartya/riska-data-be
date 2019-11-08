c31fb1bdfc11ccc9b0b75b64f4f960065ddc5cb3
Fariz Hermawan <fariz.hermawan@traveloka.com>
2019-11-09 02:00:21 +0700

    Wrap API into version package

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,14 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
- 
-Route::group([
-    'middleware' => 'api', 
-    'prefix' => 'auth'
-], function () {
-  Route::get('login', 'AuthController@login');
-  Route::get('callback', 'AuthController@callback');
-  Route::get('logout', 'AuthController@logout');
-  Route::get('refresh', 'AuthController@refresh');
-  Route::get('me', 'AuthController@me');
+
+Route::group(['prefix' => 'v1', 'namespace'  => 'v1'], function () {
+
+  Route::group(['prefix' => 'auth', 'middleware' => 'api'], function () {
+    Route::get('login', 'AuthController@login');
+    Route::get('callback', 'AuthController@callback');
+    Route::get('logout', 'AuthController@logout');
+    Route::get('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
+  });
 });
