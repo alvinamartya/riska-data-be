9116f9845b631719dc2b57155cc3f7e654a280f9
Fariz Hermawan <farizhermawan.id@gmail.com>
2020-06-14 22:48:50 +0700

    friendlier message for confirmation code

--- app/Http/Controllers/v1/AuthController.php
+++ app/Http/Controllers/v1/AuthController.php
@@ -87,7 +87,7 @@ class AuthController extends Controller
       $outbox = new WhatsappOutbox();
       $outbox->owner = config('services.whatsapp.client_id');
       $outbox->to = "{$id}@c.us";
-      $outbox->message = "Kode login kamu *$pin[0] $pin[1] $pin[2] $pin[3] $pin[4] $pin[5]*\nKode tersebut hanya berlaku 30 menit ya.";
+      $outbox->message = "Ssst, ini rahasia ya 🤫!\n\nKode login kamu *$pin[0] $pin[1] $pin[2] $pin[3] $pin[4] $pin[5]*\nKode tersebut hanya berlaku 30 menit.";
       $outbox->option = null;
       $outbox->status = WhatsappOutboxStatus::PENDING;
       $outbox->save();
