e1faac96282db2fd8aa603c056d60388f54b359c
Azis <developer.azis@gmail.com>
2020-06-21 01:07:40 +0700

    create migration talent table

---
+++ database/migrations/2020_06_21_005825_create_talent_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTalentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('talent', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string('created_by', 100);
+            $table->string('updated_by', 100)->nullable();
+            $table->string('deleted_by', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('talent');
+    }
+}
