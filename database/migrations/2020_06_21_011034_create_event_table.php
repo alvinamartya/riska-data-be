4722853c0ee6f47f7d2833628afdb7c64eb6e85d
Azis <developer.azis@gmail.com>
2020-06-21 01:14:52 +0700

    create migration event table

---
+++ database/migrations/2020_06_21_011034_create_event_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('event_name');
+            $table->date('event_date')->nullablle();
+            $table->string('created_by', 100);
+            $table->string('updated_by', 100)->nullable();
+            $table->string('deleted_by', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
