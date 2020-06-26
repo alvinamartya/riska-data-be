7a267efc514263cd5011dab767d7d3f8888295ca
Fariz Hermawan <farizhermawan.id@gmail.com>
2020-06-26 17:19:49 +0700

    Modify Suggestion group API

--- app/Http/Controllers/v1/SuggestionGroupController.php
+++ app/Http/Controllers/v1/SuggestionGroupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use App\Http\RestResponse;
 use App\Models\Suggestion;
+use Illuminate\Http\Request;
 
 class SuggestionGroupController extends Controller
 {
@@ -14,11 +15,47 @@ class SuggestionGroupController extends Controller
     return RestResponse::data(Suggestion::selectRaw("`group`, count(item) as total")->groupBy("group")->orderBy('group')->get());
   }
 
-  public function show($group)
+  public function store(Request $request)
+  {
+    if (Suggestion::whereId($request->group)->exists()) {
+      return RestResponse::conflict("group already exists!");
+    }
+
+    foreach ($request->items as $item) {
+      $suggestion = new Suggestion();
+      $suggestion->group = $request->group;
+      $suggestion->item = $item->item;
+      $suggestion->display_text = $item->display_text;
+      $suggestion->save();
+    }
+
+    return RestResponse::created($suggestion);
+  }
+
+  public function show($suggestion_group)
   {
     return RestResponse::data([
-      'group' => $group,
-      'items' => Suggestion::whereGroup($group)->get(['id', 'item', 'display_text'])
+      'group' => $suggestion_group,
+      'items' => Suggestion::whereGroup($suggestion_group)->get(['id', 'item', 'display_text'])
     ]);
+  }
+
+  public function update(Request $request, $suggestion_group)
+  {
+    if (!Suggestion::whereId($suggestion_group)->exists()) {
+      return RestResponse::conflict("group not exists!");
+    }
+
+    Suggestion::whereGroup($suggestion_group)->delete();
+
+    foreach ($request->items as $item) {
+      $suggestion = new Suggestion();
+      $suggestion->group = $suggestion_group;
+      $suggestion->item = $item->item;
+      $suggestion->display_text = $item->display_text;
+      $suggestion->save();
+    }
+
+    return RestResponse::updated($suggestion);
   }
 }
